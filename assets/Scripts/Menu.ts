5e3a8e6999c35cbc68b940d2da55b98b3417fcd4
111062123 <joan040802@gmail.com>
2024-05-10 01:42:15 +0800

    add frontend of menu and login

--- assets/Scripts/Menu.ts
+++ assets/Scripts/Menu.ts
@@ -1,14 +1,29 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, director, find, Button, EventHandler } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Menu')
 export class Menu extends Component {
     start() {
+        let loginButton = new EventHandler();
+        loginButton.target = this.node;
+        loginButton.component = "Menu";
+        loginButton.handler = "loadLoginScene";
+        find("Canvas/Background/LoginButton").getComponent(Button).clickEvents.push(loginButton);
 
+        let registerButton = new EventHandler();
+        registerButton.target = this.node;
+        registerButton.component = "Menu";
+        registerButton.handler = "loadRegisterScene";
+        find("Canvas/Background/RegisterButton").getComponent(Button).clickEvents.push(registerButton);
     }
-
-    update(deltaTime: number) {
-        
+    
+    loadLoginScene(){
+        console.log("loadLoginScene");
+        director.loadScene("login");
+    }
+    loadRegisterScene(){
+        console.log("loadRegisterScene");
+        director.loadScene("register");
     }
 }
 
